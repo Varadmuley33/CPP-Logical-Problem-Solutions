7cf50cd429b847e2c2d9f0c1eba64c4d3db58c77
Varad Muley <varad.muley408@gmail.com>
2026-04-20 22:29:30 +0530

    Update program110.cpp

---
+++ program110.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int Arr[] = {10,20,30,40};
+
+    cout << Arr[0] << endl;
+    cout << Arr[1] << endl;
+    cout << Arr[2] << endl;
+    cout << Arr[3] << endl;
+
+    return 0;
+}
