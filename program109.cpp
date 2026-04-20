8b56f2544559322d918ccb7f9143e6cb8507eced
Varad Muley <varad.muley408@gmail.com>
2026-04-20 22:29:16 +0530

    Update program109.cpp

---
+++ program109.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int Arr[] = {10,20,30,40};
+
+    cout << Arr << endl;
+    cout << &Arr << endl;
+    cout << &Arr[0] << endl;
+    cout << sizeof(Arr) << endl;
+
+    return 0;
+}
