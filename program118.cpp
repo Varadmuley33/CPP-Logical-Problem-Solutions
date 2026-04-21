c5826fb55d3b85ce1f3a92b1422901ecbae65ebd
Varad Muley <varad.muley408@gmail.com>
2026-04-21 23:09:54 +0530

    Update program118.cpp

---
+++ program118.cpp
@@ -0,0 +1,40 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     program118.cpp
+//  Description :   Print value of array using loop 
+//  Author :        Varad Nitin Muley
+//  Date :          13/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+/////////////////////////////////////////////////////////////////
+// 
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+#include <iostream>
+using namespace std;
+
+
+void Display(int Arr[])
+{
+    int iCnt = 0 ;
+
+    for (iCnt = 0 ; iCnt < 4 ; iCnt++)
+    {
+        cout << Arr[iCnt] << endl;                  
+    }
+}
+
+int main ()
+{
+    int Brr[] = {10,20,30,40};  
+
+    Display(Brr);
+    
+
+    return 0;
+}// End of main
