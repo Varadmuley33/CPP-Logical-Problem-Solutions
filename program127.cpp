e4ab690a7f5eaf0c8a2d3e481bc11e33d71dd9bb
Varad Muley <varad.muley408@gmail.com>
2026-04-22 23:42:31 +0530

    Update program127.cpp

---
+++ program127.cpp
@@ -0,0 +1,61 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     program127.c
+//  Description :   Print value of array using loop 
+//  Author :        Varad Nitin Muley
+//  Date :          13/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+/////////////////////////////////////////////////////////////////
+// 
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+
+void Display(int Arr[],int iSize)
+{
+    int iCnt = 0 ;
+
+    for (iCnt = 0 ; iCnt < iSize ; iCnt++)
+    {
+        cout << Arr[iCnt] << "\n";                  
+    }
+}
+
+int main ()
+{
+    int iLength = 0 , iCnt = 0 ;
+    int *ptr = NULL;
+
+
+    cout << "Enter number of elements : ";
+    cin >> iLength;
+
+    ptr = (int *)malloc(iLength * sizeof(int));
+    if(NULL = ptr)      // Error
+    {
+        cout << "Unable to allocate the memory\n";
+        return -1;
+    }
+
+    cout << "Enter the elements : \n";
+    
+    for(iCnt = 0 ; iCnt < iLength ; iCnt++)
+    {
+        cin >> ptr[iCnt];
+    }
+
+    Display (ptr,iLength);
+
+    free(ptr);
+
+    return 0;
+}// End of main
