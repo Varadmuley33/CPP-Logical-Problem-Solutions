ac16c320dabff0874b96003b93f3d581454b19a4
Varad Muley <varad.muley408@gmail.com>
2026-04-22 23:41:12 +0530

    Update program121.cpp

---
+++ program121.cpp
@@ -0,0 +1,40 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     program121.c
+//  Description :   Print value of array using loop 
+//  Author :        Varad Nitin Muley
+//  Date :          13/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+/////////////////////////////////////////////////////////////////
+// 
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+#include <iostream>
+using namespace std;
+
+
+void Display(int Arr[])
+{
+    int iCnt = 0 ;
+
+    for (iCnt = 0 ; iCnt < 7 ; iCnt++)
+    {
+        cout << Arr[iCnt] << "\n";                  
+    }
+}
+
+int main ()
+{
+    int Brr[] = {10,20,30,40};     
+ 
+    Display(Brr);
+    
+
+    return 0;
+}// End of main
