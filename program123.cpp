86e568d04a94edb93f8af486935e4674ca64270f
Varad Muley <varad.muley408@gmail.com>
2026-04-22 23:41:33 +0530

    Update program123.cpp

---
+++ program123.cpp
@@ -0,0 +1,56 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     program123.c
+//  Description :   Print value of array using loop 
+//  Author :        Varad Nitin Muley
+//  Date :          13/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+/////////////////////////////////////////////////////////////////
+// 
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+
+void Display(int Arr[],int iSize)
+{
+    int iCnt = 0 ;
+
+    for (iCnt = 0 ; iCnt < iSize ; iCnt++)
+    {
+        cout << Arr[iCnt] << "\n";                  
+    }
+}
+
+int main ()
+{
+    int iLength = 0 , iCnt = 0 ;
+    int *ptr = NULL;
+
+
+    cout << "Enter number of elements : ";
+    cin >> iLength;
+
+    ptr = (int *)malloc(iLength * sizeof(int));
+
+    cout << "Enter the elements : \n";
+    
+    for(iCnt = 0 ; iCnt < iLength ; iCnt++)
+    {
+        cin >> ptr[iCnt];
+    }
+
+    Display (ptr,iLength);
+
+    free(ptr);
+
+    return 0;
+}// End of main
