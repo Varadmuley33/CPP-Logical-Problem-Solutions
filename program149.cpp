eef2db8f2052224e863c1e770a12833e04dd33c8
Varad Muley <varad.muley408@gmail.com>
2026-04-25 23:41:22 +0530

    Update program149.cpp

---
+++ program149.cpp
@@ -0,0 +1,44 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Program147.cpp
+//  Author :        Varad Nitin Muley
+//  Date :          14/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+/////////////////////////////////////////////////////////////////
+// 
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+void Swap (int *p , int *q)
+{
+    int Temp = 0;
+
+    Temp = *p;
+    *p = *q;
+    *q = Temp;
+}
+
+int main ()
+{
+    
+    int iValue1 = 11;
+    int iValue2 = 21;
+
+    cout<<"Before swap "<<iValue1<<" "<<iValue2<<"\n";
+
+    Swap(&iValue1,&iValue2);
+
+    cout<<"After swap "<<iValue1<<" "<<iValue2<<"\n";
+
+    return 0 ;
+
+}
