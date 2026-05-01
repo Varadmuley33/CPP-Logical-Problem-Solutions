0c69f3bce09885784885510b0d5bfe51c313e9fb
Varad Muley <varad.muley408@gmail.com>
2026-05-01 22:27:13 +0530

    Update program165.cpp

---
+++ program165.cpp
@@ -0,0 +1,86 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Program165.cpp
+//  Description :   Prints rectangular matrix pattern of '$' using rows and columns
+//  Author :        Varad Nitin Muley
+//  Date :          18/11/2025
+//  Input :         iRow = 4, iCol = 4
+//  Output :        $ $ $ $
+//                  $ $ $ $
+//                  $ $ $ $
+//                  $ $ $ $
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+#include<iostream>
+using namespace std;
+
+/////////////////////////////////////////////////////////////////
+//
+//  Function Name : Display
+//  Description   : Prints matrix pattern of $
+//  Input         : Two Integers
+//  Output        : None
+//  Author        : Varad Nitin Muley
+//  Date          : 18/11/2025
+//
+/////////////////////////////////////////////////////////////////
+void Display(int iRow,int iCol)
+{
+    int i = 0 , j = 0 ;
+
+    for(i = 1 ; i <= iRow ; i++)
+    {
+        for (j = 1 ; j <= iCol ; j++)
+        {
+            cout<<"$\t";
+        }
+        cout<<"\n";
+    }
+}
+
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function
+//
+/////////////////////////////////////////////////////////////////
+int main()
+{
+    int iValue1 = 0 ,iValue2 = 0;
+        
+    cout<<"Enter the number of rows : \n";
+    cin>>iValue1;
+
+    cout<<"Enter the number of columns : \n";
+    cin>>iValue2;
+
+    Display(iValue1,iValue2);
+
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////
+//
+//  Test Cases
+//
+//  Input  : 4 4
+//  Output : $ $ $ $
+//           $ $ $ $
+//           $ $ $ $
+//           $ $ $ $
+//
+//  Input  : 2 3
+//  Output : $ $ $
+//           $ $ $
+//
+//  Input  : 3 2
+//  Output : $ $
+//           $ $
+//           $ $
+//
+/////////////////////////////////////////////////////////////////
