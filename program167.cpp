e3a736b9ae09489fe6543baa2f0bcde0be1c5e8c
Varad Muley <varad.muley408@gmail.com>
2026-05-01 22:27:51 +0530

    Update program167.cpp

---
+++ program167.cpp
@@ -0,0 +1,88 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Program167.cpp
+//  Description :   Prints row-wise increasing numbers pattern for each row
+//  Author :        Varad Nitin Muley
+//  Date :          18/11/2025
+//  Input :         iRow = 4, iCol = 4
+//  Output :        1 1 1 1
+//                  2 2 2 2
+//                  3 3 3 3
+//                  4 4 4 4
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+#include<iostream>
+using namespace std;
+
+/////////////////////////////////////////////////////////////////
+//
+//  Function Name : Display
+//  Description   : Prints row-wise numbers pattern
+//  Input         : Two Integers
+//  Output        : None
+//  Author        : Varad Nitin Muley
+//  Date          : 18/11/2025
+//
+/////////////////////////////////////////////////////////////////
+void Display(int iRow,int iCol)
+{
+    int i = 0 ,j = 0;
+ 
+    for (i = 1 ; i <= iRow ; i++)
+    {
+        for (j = 1 ; j <= iCol ; j++)
+        {
+            cout<<i<<"\t";
+        }
+        cout<<"\n";
+    }
+   
+    cout<<"\n"; 
+}
+
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function
+//
+/////////////////////////////////////////////////////////////////
+int main()
+{
+    int iValue1 = 0 ,iValue2 = 0 ;
+
+    cout<<"Enter the number of rows : \n";
+    cin>>iValue1;
+
+    cout<<"Enter the number of columns : \n";
+    cin>>iValue2;
+
+    Display(iValue1,iValue2);
+
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////
+//
+//  Test Cases
+//
+//  Input  : 4 4
+//  Output : 1 1 1 1
+//           2 2 2 2
+//           3 3 3 3
+//           4 4 4 4
+//
+//  Input  : 2 3
+//  Output : 1 1 1
+//           2 2 2
+//
+//  Input  : 3 2
+//  Output : 1 1
+//           2 2
+//           3 3
+//
+/////////////////////////////////////////////////////////////////
