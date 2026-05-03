f1ed593a3a9a5de0c28d72173f72fff1f00e3532
Varad Muley <varad.muley408@gmail.com>
2026-05-03 23:39:51 +0530

    Update program186.cpp

---
+++ program186.cpp
@@ -0,0 +1,101 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     Program186.cpp
+//  Description :   Prints diagonal pattern with '$' on main diagonal and '*' elsewhere
+//  Author :        Varad Nitin Muley
+//  Date :          18/11/2025
+//  Input :         6
+//  Output :        
+//       
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+//
+//  Required Header files
+//
+/////////////////////////////////////////////////////////////////
+#include<iostream>
+using namespace std;
+
+/////////////////////////////////////////////////////////////////
+//
+//  Function name : Display
+//  Description   : Displays diagonal pattern
+//  Input         : Integer, Integer
+//  Output        : void
+//  Author        : Varad Nitin Muley
+//  Date          : 18/11/2025
+//
+/////////////////////////////////////////////////////////////////
+
+void Display(int iRow,int iCol)
+{
+    int i = 0 ,j = 0;
+    int iFirst = 0, iLast = 0;
+
+    for (i = 1 ; i <= iRow ; i++)
+    {
+        for (j = 1 ; j <= iCol ; j++)
+        {
+            if((i == j))
+            {
+                cout<<"$\t";
+            }
+            else
+            {
+                cout<<"*\t";
+            }
+        }
+        cout<<"\n";
+    }
+
+    cout<<"\n"; 
+}
+
+/////////////////////////////////////////////////////////////////
+//
+//  Entry point function
+//
+/////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0 ,iValue2 = 0 ;
+
+    cout<<"Enter the number of rows : \n";
+    cin>>iValue1;
+
+    cout<<"Enter the number of columns : \n";
+    cin>>iValue2;
+
+    Display(iValue1,iValue2);
+
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////
+//
+//  Testcases :
+//
+//  Input  : 4 4
+//  Output : 
+//           $ * * *
+//           * $ * *
+//           * * $ *
+//           * * * $
+//
+//  Input  : 3 3
+//  Output : 
+//           $ * *
+//           * $ *
+//           * * $
+//
+//  Input  : 5 5
+//  Output : 
+//           $ * * * *
+//           * $ * * *
+//           * * $ * *
+//           * * * $ *
+//           * * * * $
+//
+/////////////////////////////////////////////////////////////////
