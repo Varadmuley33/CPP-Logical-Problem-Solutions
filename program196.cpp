1d10b0c6191b0725bbe81ca75557c063abf4d4cf
Varad Muley <varad.muley408@gmail.com>
2026-05-04 23:04:52 +0530

    Update program196.cpp

---
+++ program196.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<fcntl.h>
+#include<unistd.h>
+using namespace std;
+
+int main ()
+{
+    int fd = 0 ;
+    int iRet = 0 ;
+
+    fd = creat("PPA.txt",0777);
+
+    if(fd == -1)
+    {
+        cout<<"Unable to create the file\n";
+    }
+    else
+    {
+        cout<<"File successfully created\n";
+    }
+
+    return 0;
+}
