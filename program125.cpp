055aaa07d6861ac7af3eab2631564e51839ae64d
Varad Muley <varad.muley408@gmail.com>
2026-09-04 23:11:54 +0530

    Fix formatting and comments in program125.cpp

--- program125.cpp
+++ program125.cpp
@@ -2,7 +2,7 @@
 //
 //  File name :     program125.c
 //  Description :   Print value of array using loop 
-//  Author :        Varad Nitin Muley
+//  Author :        Varad Nitin Muley   
 //  Date :          13/11/2025
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////
