d1019d90c56c30242e59cf39b476e14e26294216
Varad Muley <varad.muley408@gmail.com>
2026-09-16 23:20:38 +0530

    Remove redundant comments and clean up formatting

--- program191.cpp
+++ program191.cpp
@@ -4,7 +4,7 @@
 //  Description :   Prints matrix with '%' on diagonal, '*' below and '#' above
 //  Author :        Varad Nitin Muley
 //  Date :          18/11/2025
-//  Input :         6
+//  Input :         6  
 //  Output :        
 //       
 ////////////////////////////////////////////////////////////////////////////////////////////////
