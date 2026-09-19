dd6fa75726a6b05f719df8fe6f384eb08d5594aa
Varad Muley <varad.muley408@gmail.com>
2026-09-19 22:45:49 +0530

    Remove redundant comments in Program29.cpp

--- program29.cpp
+++ program29.cpp
@@ -3,7 +3,7 @@
 //  File name :     Program29.cpp
 //  Description :   Demonstrates the use of a loop to display numbers
 //                  from 1 to 5 using a function in C.
-//  Author :        Varad Nitin Muley
+//  Author :        Varad Nitin Muley   
 //  Date :          18/11/2025
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////
