5fb7dad75b939a17bb43212daa427064d7425c2b
Varad Muley <varad.muley408@gmail.com>
2026-09-21 23:51:03 +0530

    Remove redundant comments and clean up code

--- program190.cpp
+++ program190.cpp
@@ -3,7 +3,7 @@
 //  File name :     Program190.cpp
 //  Description :   Prints upper triangular '#' and lower '*' pattern for square matrix
 //  Author :        Varad Nitin Muley
-//  Date :          18/11/2025
+//  Date :          18/11/2025   
 //  Input :         6
 //  Output :        
 //       
